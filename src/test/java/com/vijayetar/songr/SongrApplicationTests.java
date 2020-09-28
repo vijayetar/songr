79f85f06d4eb3a99615307d5d5545df9acf0eff8
Vijayeta Rangarajan <vijayetar@gmail.com>
2020-09-28 15:58:26 -0700

    first commit

---
+++ src/test/java/com/vijayetar/songr/SongrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vijayetar.songr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
