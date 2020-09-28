113b8325f9d4d9005e692f180501cd4542183d68
Vijayeta Rangarajan <vijayetar@gmail.com>
2020-09-28 16:29:09 -0700

    got capitalize to work

--- src/main/java/com/vijayetar/songr/HomeController.java
+++ src/main/java/com/vijayetar/songr/HomeController.java
@@ -1,13 +1,24 @@
 package com.vijayetar.songr;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
     public String showHome(){
         return "home";
+    }
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "hello";
+    }
+    @GetMapping("/capitalize/{message}")
+    public String capitalize(Model m, @PathVariable String message){
+        m.addAttribute("message", message.toUpperCase());
+        return "capitalize";
     }
 
 
