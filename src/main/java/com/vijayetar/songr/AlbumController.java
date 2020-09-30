baf9ee41462e36018bda232feedfd130d407d95b
Vijayeta Rangarajan <vijayetar@gmail.com>
2020-09-30 16:28:36 -0700

    lab12 completed

--- src/main/java/com/vijayetar/songr/AlbumController.java
+++ src/main/java/com/vijayetar/songr/AlbumController.java
@@ -1,6 +1,7 @@
 package com.vijayetar.songr;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.converter.json.GsonBuilderUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,13 +18,14 @@ public class AlbumController {
     @PostMapping("/album") // app.post('/emotion')
     public RedirectView addAlbum(String title, String artist, int songCount, int length, String imageUrl, String color){
         Album newAlbum = new Album ( title, artist, songCount, length, imageUrl, color);
+        Album testAlbum = new Album ("test","test",1,1,"test","test");
         albumRepository.save(newAlbum);
         return new RedirectView("/albums");
     }
     @GetMapping("/albums")
     public String showAlbums(Model m){
 
-        ArrayList<Album> albums = (ArrayList<Album>) albumRepository.findAll();
+        ArrayList<Album> albums = (ArrayList<Album>)albumRepository.findAll();
 
         m.addAttribute("albums", albums);
         return "albums";
